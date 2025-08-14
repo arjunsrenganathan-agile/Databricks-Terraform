440278f8ff6353f8edb097ad14f025051594f553
arjunsrenganathan-agile <arjuns.renganathan@gmail.com>
2025-08-15 09:42:47 +1000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,22 +1,26 @@
+# Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3.0.2"
     }
     databricks = {
       source  = "databricks/databricks"
-      version = "~> 1.0"
+      version = "~> 1.0.0"
     }
   }
-  required_version = ">= 1.0"
+
+  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
   features {}
+  # Authentication will be handled by GitHub Actions environment variables
 }
 
-# Configure Databricks provider
+# Configure the Databricks provider using Azure authentication
 provider "databricks" {
-  host = azurerm_databricks_workspace.main.workspace_url
+  host = azurerm_databricks_workspace.databricks.workspace_url
+  # Authentication will be handled by GitHub Actions environment variables
 }
